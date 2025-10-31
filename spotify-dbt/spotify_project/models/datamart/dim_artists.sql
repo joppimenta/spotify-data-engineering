04d01bb286edf961fdc1d36d541b04c420ed8f07
jop_pimenta <joaopedropimenta@alu.ufc.br>
2025-10-30 21:47:03 -0300

    finishing project v1

--- spotify-dbt/spotify_project/models/datamart/dim_artists.sql
+++ spotify-dbt/spotify_project/models/datamart/dim_artists.sql
@@ -9,13 +9,3 @@ SELECT DISTINCT
     artist_type,
     artist_spotify
 FROM {{ ref('track_to_artists') }}
-
-UNION DISTINCT
-
-SELECT DISTINCT
-    artist_id,
-    artist_name,
-    artist_href,
-    artist_type,
-    CAST(NULL AS STRING) AS artist_spotify
-FROM {{ ref('album_to_artists') }}
